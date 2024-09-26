c4cd2e681bebeab2f4d1ddd393f2db7e8c963d13
charlo-faucher <fauchercharlo@gmail.com>
2024-09-26 10:13:34 -0400

    Add dummy server

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const axios = require('axios');
+const express = require('express');
 require('dotenv').config();
+
+// Dummy express server to stop Render port warnings
+const app = express();
+const port = process.env.PORT || 3000;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 const powerAutomateUrl = process.env.POWER_AUTOMATE_URL; // L'URL générée par Power Automate
@@ -22,3 +27,9 @@ client.on('messageCreate', (message) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+app.get('/', (req, res) => res.send('Bot is running!'));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
