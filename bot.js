cabfded5ac98f73f026cea52827cfa3ddd6b28ae
charlo-faucher <fauchercharlo@gmail.com>
2024-10-01 18:25:04 -0400

    Error handling

--- bot.js
+++ bot.js
@@ -5,10 +5,20 @@ require('dotenv').config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 const powerAutomateUrl = process.env.POWER_AUTOMATE_URL; // L'URL générée par Power Automate
 
+await client.login(process.env.DISCORD_TOKEN);
+
 client.once('ready', () => {
     console.log('Bot connecté à Discord !');
 });
 
+client.on('disconnect', async () => {
+    console.log('Bot disconnected, attempting to reconnect...');
+    await client.login(process.env.DISCORD_TOKEN);
+});
+
+client.on('reconnecting', () => {
+    console.log('Reconnecting...');
+});
 
 client.on('messageCreate', (message) => {
     if (message.channelId === process.env.CHANNEL_ID && !message.author.bot) {
@@ -20,5 +30,3 @@ client.on('messageCreate', (message) => {
             .catch(error => console.error('Erreur lors du transfert du message :', error));
     }
 });
-
-client.login(process.env.DISCORD_TOKEN);
